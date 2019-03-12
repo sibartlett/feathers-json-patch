516cb6c53aa7bcb34923b427c4257dac5780f111
Simon Bartlett <simon@sibartlett.com>
2019-03-12 12:17:30 -0400

    Use patch instead of update

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const { applyPatch } = require("fast-json-patch");
 const wrap = service => {
   if (
     typeof service.get !== "function" ||
-    typeof service.patch !== "function" ||
-    typeof service.update !== "function"
+    typeof service.patch !== "function"
   ) {
     throw new Error(
-      "feathers-json-patch expects services to implement get, patch and update methods"
+      "feathers-json-patch expects services to implement get and patch methods"
     );
   }
 
   const newService = {
     get: (id, params) => service.get(id, params),
-    update: (id, data, params) => service.update(id, data, params),
     patch: (id, data, params) => {
       if (id && Array.isArray(data)) {
         return service
           .get(id, params)
           .then(document => applyPatch(document, data).newDocument)
-          .then(document => service.update(id, document, params));
+          .then(document => service.patch(id, document, params));
       } else {
         return service.patch(id, data, params);
       }
@@ -32,6 +30,10 @@ const wrap = service => {
 
   if (typeof service.create === "function") {
     newService.create = (data, params) => service.create(data, params);
+  }
+
+  if (typeof service.update === "function") {
+    newService.update = (id, data, params) => service.update(id, data, params);
   }
 
   if (typeof service.remove === "function") {
